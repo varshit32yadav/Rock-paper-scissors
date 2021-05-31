469a6f17bd7a335bab3545d619f4edbb60aef44f
varshit singh yadav <singhvarshit602@gail.com>
2021-05-31 20:34:10 +0530

    play again

--- app1.js
+++ app1.js
@@ -15,6 +15,7 @@ const game = () => {
     };
     //Play Match
     const playMatch = () => {
+      const winner = document.querySelector(".winner");
       const options = document.querySelectorAll(".options button");
       const playerHand = document.querySelector(".player-hand");
       const computerHand = document.querySelector(".computer-hand");
@@ -50,22 +51,54 @@ const game = () => {
         });
       });
     };
-  
     const updateScore = () => {
+      const winner = document.querySelector(".winner");
+      const playerHand = document.querySelector(".player-hand");
+      const computerHand = document.querySelector(".computer-hand");
+      const neww = document.querySelector(".neww");
       const playerScore = document.querySelector(".player-score p");
       const computerScore = document.querySelector(".computer-score p");
       playerScore.textContent = pScore;
       computerScore.textContent = cScore;
+      setTimeout(()=>{
+        if(pScore ==2)
+      {
+        game(); neww.textContent="player wins the match"; window.alert("HARD LUCK!! PLAYER WON LET'S START NEW MATCH"); 
+        setTimeout(()=>{
+          pScore=0;cScore=0; 
+          playerHand.src = `./assets/rock.png`;
+          computerHand.src = `./assets/rock.png`;
+          winner.textContent="Let's Start";
+          updateScore();return;
+        },1000);
+       
+      }
+      else if(cScore ==2)
+      {
+        game(); neww.textContent="computer wins the match"; window.alert("HARD LUCK!! COMPUTER WON LET'S START NEW MATCH"); 
+        setTimeout(()=>{
+          pScore=0;cScore=0; 
+          playerHand.src = `./assets/rock.png`;
+          computerHand.src = `./assets/rock.png`;
+          winner.textContent="Let's Start";
+          updateScore();return;
+        },900);
+      }
+      
+      },100);
+      
+     
     };
   
     const compareHands = (playerChoice, computerChoice) => {
       //Update Text
       const winner = document.querySelector(".winner");
-      //Checking for a tie
-      if (playerChoice === computerChoice) {
-        winner.textContent = "It is a tie";
-        return;
-      }
+      //new match
+     //Checking for a tie
+     if (playerChoice === computerChoice) {
+      winner.textContent = "It is a tie" ;
+      return;
+    }
       //Check for Rock
       if (playerChoice === "rock") {
         if (computerChoice === "scissors") {
